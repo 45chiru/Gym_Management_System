e5413d1b3d082909a1351bfb0fd47ad5c231bb77
45chiru <chiranjibisahu007@gmail.com>
2024-06-22 09:55:55 +0530

    change index

---
+++ script.js
@@ -0,0 +1,19 @@
+const home = document.getElementById("home");
+home.addEventListener("click", function() {
+  window.location.href = "home.html"; 
+});
+const about = document.getElementById("about");
+about.addEventListener("click", function() {
+  window.location.href = "about.html"
+});
+
+const classes = document.getElementById("classes");
+classes.addEventListener("click", function() {
+  window.location.href = "classes.html"
+});
+
+const contact = document.getElementById("contact");
+contact.addEventListener("click", function() {
+  window.location.href = "contact.html"
+});
+
